e4acab9f34f6b22c1c97b321b4950a42c9a62919
Kir Belevich <kir@belevi.ch>
2017-08-19 13:40:11 +0200

    :boom: :wrench: with-log: refactor using "eager factory"

--- packages/with-log/src/index.js
+++ packages/with-log/src/index.js
@@ -1,20 +1,17 @@
 /* eslint-disable no-console */
-import React from 'react';
-import { setDisplayName, wrapDisplayName } from 'recompose';
+import { createEagerFactory, setDisplayName, wrapDisplayName } from 'recompose';
 
 const withLog = (getMessageToLog = (props) => props) => (Target) => {
   if (process.env.NODE_ENV === 'production') {
     return Target;
   }
 
+  const factory = createEagerFactory(Target);
   const displayName = wrapDisplayName(Target, 'withLog');
-
   const WithLog = (props) => {
     console.log(`${displayName}:`, getMessageToLog(props));
 
-    return (
-      <Target {...props}/>
-    );
+    return factory(props);
   };
 
   return setDisplayName(displayName)(WithLog);
