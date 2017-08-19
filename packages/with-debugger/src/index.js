266c4b64d7310c2505820138512e628674428240
Kir Belevich <kir@belevi.ch>
2017-08-19 13:45:18 +0200

    :boom: :wrench: with-debugger: refactor using "eager factory"

--- packages/with-debugger/src/index.js
+++ packages/with-debugger/src/index.js
@@ -1,18 +1,16 @@
 /* eslint-disable no-debugger */
-import React from 'react';
-import { setDisplayName, wrapDisplayName } from 'recompose';
+import { createEagerFactory, setDisplayName, wrapDisplayName } from 'recompose';
 
 const withDebugger = (Target) => {
   if (process.env.NODE_ENV === 'production') {
     return Target;
   }
 
+  const factory = createEagerFactory(Target);
   const WithDebugger = (props) => {
     debugger;
 
-    return (
-      <Target {...props}/>
-    );
+    return factory(props);
   };
 
   return setDisplayName(wrapDisplayName(Target, 'withDebugger'))(WithDebugger);
