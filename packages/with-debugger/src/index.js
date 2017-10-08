25c9f5b7226bdff9dc968102a1fed15bcb94e6ea
Kir Belevich <kir@belevi.ch>
2017-10-08 17:35:05 +0200

    :recycle: :wrench: with-debugger: bump recompose and drop eager factory usage

--- packages/with-debugger/src/index.js
+++ packages/with-debugger/src/index.js
@@ -1,16 +1,16 @@
 /* eslint-disable no-debugger */
-import { createEagerFactory, setDisplayName, wrapDisplayName } from 'recompose'
+import { createElement } from 'react'
+import { setDisplayName, wrapDisplayName } from 'recompose'
 
 const withDebugger = (Target) => {
   if (process.env.NODE_ENV === 'production') {
     return Target
   }
 
-  const factory = createEagerFactory(Target)
   const WithDebugger = (props) => {
     debugger
 
-    return factory(props)
+    return createElement(Target, props)
   }
 
   return setDisplayName(wrapDisplayName(Target, 'withDebugger'))(WithDebugger)
