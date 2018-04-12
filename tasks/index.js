a49069f45fbf216ca8bb9d857ba4b7f00df224ba
Kir Belevich <kir@belevi.ch>
2018-04-12 22:04:33 +0200

    ♻️ bump deps

--- tasks/index.js
+++ tasks/index.js
@@ -1,105 +1,94 @@
-import Task from '@start/task'
-import Reporter from '@start/reporter'
+import sequence from '@start/plugin-sequence'
+import parallel from '@start/plugin-parallel'
 import env from '@start/plugin-env'
 import find from '@start/plugin-find'
-import clean from '@start/plugin-clean'
+import remove from '@start/plugin-remove'
 import read from '@start/plugin-read'
-import babel from '@start/plugin-babel'
+import babel from '@start/plugin-lib-babel'
 import write from '@start/plugin-write'
-import parallel from '@start/plugin-parallel'
-import jest from '@start/plugin-jest'
-import eslint from '@start/plugin-eslint'
-import webpackServe from '@start/plugin-webpack-serve'
+import eslint from '@start/plugin-lib-eslint'
+import jest from '@start/plugin-lib-jest'
+import webpackServe from '@start/plugin-lib-webpack-serve'
 
-const reporter = Reporter()
-const task = Task(reporter)
+import {
+  babelConfigNodeCJS,
+  babelConfigBrowserESM,
+  babelConfigReactNative
+} from './babel/config'
+import {
+  jestConfigFull,
+  jestConfigWatch
+} from './jest/config'
 
-export const buildNodeCJS = (packageName) => {
-  const { babelConfigNodeCJS } = require('./babel/config')
-
-  return task(
+export const buildNodeCJS = (packageName) =>
+  sequence(
     find(`packages/${packageName}/src/**/*.js`),
     read,
     babel(babelConfigNodeCJS),
     write(`packages/${packageName}/build/node-cjs/`)
   )
-}
 
-export const buildBrowserESM = (packageName) => {
-  const { babelConfigBrowserESM } = require('./babel/config')
-
-  return task(
+export const buildBrowserESM = (packageName) =>
+  sequence(
     find(`packages/${packageName}/src/**/*.js`),
     read,
     babel(babelConfigBrowserESM),
     write(`packages/${packageName}/build/browser-esm/`)
   )
-}
 
-export const buildReactNative = (packageName) => {
-  const { babelConfigReactNative } = require('./babel/config')
-
-  return task(
+export const buildReactNative = (packageName) =>
+  sequence(
     find(`packages/${packageName}/src/**/*.js`),
     read,
     babel(babelConfigReactNative),
     write(`packages/${packageName}/build/react-native/`)
   )
-}
 
 export const build = (packageName) => {
   const pkg = require(`../packages/${packageName}/package.json`)
-  const targets = [
-    pkg.main && buildNodeCJS,
-    pkg.browser && buildBrowserESM,
-    pkg['react-native'] && buildReactNative
+  const tasks = [
+    pkg.main && 'buildNodeCJS',
+    pkg.browser && 'buildBrowserESM'
+    // pkg['react-native'] && 'buildReactNative'
   ].filter((target) => target)
 
-  return task(
+  return sequence(
     find(`packages/${packageName}/build/`),
-    clean,
+    remove,
     env('NODE_ENV', 'production'),
-    parallel(...targets)(packageName)
+    parallel(tasks)(packageName)
   )
 }
 
-export const lint = () => task(
-  find([
-    'tasks/**/*.js',
-    'packages/*/@(src|test|demo)/**/*.js'
-  ]),
-  eslint({
-    cache: true,
-    cacheLocation: 'node_modules/.cache/eslint'
-  })
-)
+export const lint = () =>
+  sequence(
+    find([
+      'tasks/**/*.js',
+      'packages/*/@(src|test|demo)/**/*.js'
+    ]),
+    eslint()
+  )
 
-export const test = () => {
-  const { jestConfigFull } = require('./jest/config')
-
-  return task(
+export const test = () =>
+  sequence(
     env('NODE_ENV', 'test'),
     find('coverage/'),
-    clean,
+    remove,
     jest(jestConfigFull)
   )
-}
 
-export const testWatch = () => {
-  const { jestConfigWatch } = require('./jest/config')
-
-  return task(
+export const testWatch = () =>
+  sequence(
     env('NODE_ENV', 'test'),
     find('coverage/'),
-    clean,
+    remove,
     jest(jestConfigWatch)
   )
-}
 
 export const demo = (packageName) => {
   const webpackConfig = require('./demo/config').default
 
-  return task(
+  return sequence(
     env('NODE_ENV', 'development'),
     webpackServe({ config: webpackConfig(packageName) })
   )
