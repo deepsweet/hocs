698c09cc482f0aa2b7bf7cea8fa1f1f3e1433fc9
Kir Belevich <kir@belevi.ch>
2018-04-23 22:06:15 +0200

    ♻️ bump and sync with next Start

--- tasks/index.js
+++ tasks/index.js
@@ -9,47 +9,47 @@ import write from '@start/plugin-write'
 import eslint from '@start/plugin-lib-eslint'
 import jest from '@start/plugin-lib-jest'
 import webpackServe from '@start/plugin-lib-webpack-serve'
+import codecov from '@start/plugin-lib-codecov'
 
-import {
-  babelConfigNodeCJS,
-  babelConfigBrowserESM,
-  babelConfigReactNative
-} from './babel/config'
-import {
-  jestConfigFull,
-  jestConfigWatch
-} from './jest/config'
+export const buildNodeCJS = async (packageName) => {
+  const { default: { babelConfigNodeCJS } } = await import('./babel/config')
 
-export const buildNodeCJS = (packageName) =>
-  sequence(
+  return sequence(
     find(`packages/${packageName}/src/**/*.js`),
     read,
     babel(babelConfigNodeCJS),
     write(`packages/${packageName}/build/node-cjs/`)
   )
+}
 
-export const buildBrowserESM = (packageName) =>
-  sequence(
+export const buildBrowserESM = async (packageName) => {
+  const { default: { babelConfigBrowserESM } } = await import('./babel/config')
+
+  return sequence(
     find(`packages/${packageName}/src/**/*.js`),
     read,
     babel(babelConfigBrowserESM),
     write(`packages/${packageName}/build/browser-esm/`)
   )
+}
 
-export const buildReactNative = (packageName) =>
-  sequence(
+export const buildReactNative = async (packageName) => {
+  const { default: { babelConfigReactNative } } = await import('./babel/config')
+
+  return sequence(
     find(`packages/${packageName}/src/**/*.js`),
     read,
     babel(babelConfigReactNative),
     write(`packages/${packageName}/build/react-native/`)
   )
+}
 
-export const build = (packageName) => {
-  const pkg = require(`../packages/${packageName}/package.json`)
+export const build = async (packageName) => {
+  const pkg = await import(`../packages/${packageName}/package.json`)
   const tasks = [
     pkg.main && 'buildNodeCJS',
-    pkg.browser && 'buildBrowserESM'
-    // pkg['react-native'] && 'buildReactNative'
+    pkg.browser && 'buildBrowserESM',
+    pkg['react-native'] && 'buildReactNative'
   ].filter((target) => target)
 
   return sequence(
@@ -64,30 +64,45 @@ export const lint = () =>
   sequence(
     find([
       'tasks/**/*.js',
-      'packages/*/@(src|test|demo)/**/*.js'
+      'packages/*/{src,test,demo}/**/*.js'
     ]),
     read,
     eslint()
   )
 
-export const test = () =>
-  sequence(
+export const test = async () => {
+  const { jestConfigFull } = await import('./jest/config')
+
+  return sequence(
     env('NODE_ENV', 'test'),
     find('coverage/'),
     remove,
     jest(jestConfigFull)
   )
+}
 
-export const testWatch = () =>
-  sequence(
+export const testWatch = async () => {
+  const { jestConfigWatch } = await import('./jest/config')
+
+  return sequence(
     env('NODE_ENV', 'test'),
     find('coverage/'),
     remove,
     jest(jestConfigWatch)
   )
+}
 
-export const demo = (packageName) => {
-  const webpackConfig = require('./demo/config').default
+export const ci = () =>
+  sequence(
+    lint(),
+    test(),
+    find('coverage/lcov.info'),
+    read,
+    codecov
+  )
+
+export const demo = async (packageName) => {
+  const { default: webpackConfig } = await import('./demo/config')
 
   return sequence(
     env('NODE_ENV', 'development'),
