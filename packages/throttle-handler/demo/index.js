a4f76aa86305120c15caa1dc6a0b391fbaca5584
nmccready <nemtcan@gmail.com>
2018-08-28 16:15:51 -0400

    Demo and Documentation/Readme updates

--- packages/throttle-handler/demo/index.js
+++ packages/throttle-handler/demo/index.js
@@ -3,17 +3,46 @@ import { compose, withState, withHandlers } from 'recompose'
 
 import throttleHandler from '../src/'
 
-const Demo = ({ count, onButtonClick }) => (
-  <div>
+const Demo = ({ count, onButtonClick, label }) => (
+  <div className='demo'>
+    {label || ''}
     <h1>{count}</h1>
     <button onClick={onButtonClick}>CLICK ME FAST</button>
   </div>
 )
 
-export default compose(
+const Demo1 = compose(
   withState('count', 'setCount', 0),
   withHandlers({
     onButtonClick: ({ count, setCount }) => () => setCount(count + 1)
   }),
   throttleHandler('onButtonClick', 1000)
 )(Demo)
+
+const Demo2 = compose(
+  withState('count', 'setCount', 0),
+  withHandlers({
+    onButtonClick: ({ count, setCount }) => () => setCount(count + 1)
+  }),
+  throttleHandler('onButtonClick', (props) => props.debounce || 0)
+)(Demo)
+
+const MainDemo = () => (
+  <div>
+    <style>
+      {
+        `.demo {
+          margin-bottom: 10px;
+          border-style: dotted;
+          border-radius: 10px;
+          padding: 5px;
+        }`
+      }
+    </style>
+    <Demo1 label='Demo 1' />
+    <Demo2 label='Demo 2 w throttle' debounce={1000} />
+    <Demo2 label='Demo 2 w/o throttle' />
+  </div>
+)
+
+export default MainDemo
